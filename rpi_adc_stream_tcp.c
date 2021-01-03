79df5790d1c9d6df594df94b5d1518ff8090e250
Tim Schneider <mail@schneidertim.de>
2021-01-03 19:13:13 +0100

    chore(rpi_adc_stream_tcp.c): remove more unsued methods

--- rpi_adc_stream_tcp.c
+++ rpi_adc_stream_tcp.c
@@ -4,11 +4,6 @@
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
-#include <ctype.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <poll.h>
 #include "rpi_dma_utils.h"
 
 #define VERSION "0.1"
@@ -94,15 +89,11 @@ uint32_t rx_buff[MAX_SAMPS];
 extern MEM_MAP gpio_regs, dma_regs, clk_regs, pwm_regs;
 MEM_MAP vc_mem, spi_regs, usec_regs;
 
-// Data formats for -f option
-#define FMT_USEC        1
-
 void terminate(int sig);
 void map_devices(void);
 
 int init_spi(int hz);
 void spi_cs(int set);
-void spi_xfer(uint8_t *txd, uint8_t *rxd, int len);
 void spi_disable(void);
 
 void adc_dma_init(MEM_MAP *mp, int n_samples, int single);
@@ -110,9 +101,10 @@ void adc_stream_start(void);
 void adc_stream_wait(void);
 void adc_stream_stop(void);
 int adc_stream_csv(MEM_MAP *mp, char *value_string, int maxlen, int nsamples);
+int adc_stream_float_values(MEM_MAP *mp, uint32_t* timestamp_usec, float *values, int maxlen, int nsamples);
 void do_streaming(MEM_MAP *mp, char *vals, int maxlen, int nsamp);
 
-int in_chans=1, sample_count=0, sample_rate=SAMPLE_RATE;
+int in_chans=1, sample_count=0, sample_rate=4096;
 uint32_t lockstep;
 
 uint32_t pwm_range, overrun_total;
@@ -125,10 +117,9 @@ int main(int argc, char *argv[])
     signal(SIGINT, terminate); // Terminate on Ctrl+C
 
     pwm_range = (PWM_FREQ * 2) / sample_rate;
-    uint32_t spi_freq = init_spi(SPI_FREQ);
 
     lockstep = 0;
-    sample_count = 64;
+    sample_count = 10;
 
     printf("Streaming %u samples per block at %u S/s %s\n",
            sample_count, sample_rate, lockstep ? "(lockstep)" : "");
@@ -208,26 +199,6 @@ int init_spi(int hz)
     return(f);
 }
 
-// Set / clear SPI chip select
-void spi_cs(int set)
-{
-    uint32_t csval = *REG32(spi_regs, SPI_CS);
-
-    *REG32(spi_regs, SPI_CS) = set ? csval | 0x80 : csval & ~0x80;
-}
-
-// Transfer SPI bytes
-void spi_xfer(uint8_t *txd, uint8_t *rxd, int len)
-{
-    while (len--)
-    {
-        *REG8(spi_regs, SPI_FIFO) = *txd++;
-        while((*REG32(spi_regs, SPI_CS) & (1<<17)) == 0) ;
-        *rxd++ = *REG32(spi_regs, SPI_FIFO);
-    }
-}
-
-
 typedef struct {
     DMA_CB cbs[NUM_CBS];
     uint32_t samp_size, pwm_val, adc_csd, txd[2];
@@ -238,6 +209,8 @@ typedef struct {
 void adc_dma_init(MEM_MAP *mp, int n_samples, int single)
 {
     ADC_DMA_DATA *dp=mp->virt;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-field-initializers"
     ADC_DMA_DATA dma_data = {
             .samp_size = 2, .pwm_val = pwm_range, .txd={0xd0, in_chans>1 ? 0xf0 : 0xd0},
             .adc_csd = SPI_TFR_ACT | SPI_AUTO_CS | SPI_DMA_EN | SPI_FIFO_CLR | ADC_CE_NUM,
@@ -258,6 +231,7 @@ void adc_dma_init(MEM_MAP *mp, int n_samples, int single)
                     {PWM_TI,    MEM(mp, &dp->adc_csd),     REG(spi_regs, SPI_CS),   4,             0, CBS(7), 0}, // 9
             }
     };
+#pragma clang diagnostic pop
     if (single)                                 // If single-shot, stop after first Rx block
         dma_data.cbs[2].next_cb = 0;
     memcpy(dp, &dma_data, sizeof(dma_data));    // Copy DMA data into uncached memory
@@ -282,28 +256,33 @@ void do_streaming(MEM_MAP *mp, char *vals, int maxlen, int nsamp)
 {
     int n;
 
-    if ((n=adc_stream_csv(mp, vals, maxlen, nsamp)) > 0)
-    {
-        printf("%s", vals);
-//        if (!write_fifo(fifo_fd, vals, n))
-//        {
-//            printf("Stopped streaming\n");
-//            close(fifo_fd);
-//            fifo_fd = 0;
-//            usleep(100000);
-//        }
+    uint32_t time_usec;
+    float samples[nsamp];
+
+    char value_string [((4+1+3+1)*nsamp)+20];
+    uint32_t string_len=0;
+
+
+    if ((n=adc_stream_float_values(mp, &time_usec, samples, (uint)(sizeof samples), nsamp)) > 0){
+        string_len += sprintf(&value_string[string_len], "%d", time_usec);
+        for (int i=0; i < n; i++){
+            // print n samples (ADC-Values) comma-separated in one line
+            string_len += sprintf(&value_string[string_len], "%s%4.3f", string_len ? "," : "",
+                                  ADC_VOLTAGE(ADC_RAW_VAL(rx_buff[i])));
+        }
+        string_len += sprintf(&value_string[string_len], "\n");
+        printf("%s", value_string);
     }
     else
         usleep(10);
 }
 
-// Fetch samples from ADC buffer, return comma-delimited integer values
-// If in lockstep mode, discard new data if FIFO isn't empty
-int adc_stream_csv(MEM_MAP *mp, char *value_string, int maxlen, int nsamples)
-{
+int adc_stream_float_values(MEM_MAP *mp, uint32_t* timestamp_usec, float *values, int maxlen, int nsamples) {
     ADC_DMA_DATA *dp=mp->virt;
-    uint32_t i, n, usec, string_len=0;
-    for (n=0; n<2 && string_len == 0; n++)
+    uint32_t n, usec, buf_len=0;
+
+
+    for (n=0; n<2 && buf_len == 0; n++)
     {
         if (dp->states[n])
         {
@@ -320,28 +299,16 @@ int adc_stream_csv(MEM_MAP *mp, char *value_string, int maxlen, int nsamples)
                 usec_start = usec;
             if (!lockstep)
             {
-//              if (data_format == FMT_USEC) {
-                    string_len += sprintf(&value_string[string_len], "%u", usec - usec_start); // print timestamp since start
-//              }
-
-                for (i=0; i < nsamples && string_len + 20 < maxlen; i++){
-                    // print n samples (ADC-Values) comma-separated in one line
-                    string_len += sprintf(&value_string[string_len], "%s%4.3f", string_len ? "," : "",
-                                          ADC_VOLTAGE(ADC_RAW_VAL(rx_buff[i])));
+                *timestamp_usec = usec - usec_start;
+                for (int i = 0; i < nsamples && i < maxlen; ++i) {
+                    values[i] = ADC_VOLTAGE(ADC_RAW_VAL(rx_buff[i]));
                 }
-                string_len += sprintf(&value_string[string_len], "\n");
-
-//                if (verbose)
-//                { // print raw ADC-Values (bytes)
-//                    for (int i=0; i<nsamples*4; i++)
-//                        printf("%02X ", *(((uint8_t *)rx_buff)+i));
-//                    printf("\n");
-//                }
+                buf_len = nsamples < maxlen ? nsamples : maxlen;
             }
         }
     }
-    value_string[string_len] = 0;
-    return(string_len);
+
+    return(buf_len);
 }
 
 // Stop ADC data acquisition
